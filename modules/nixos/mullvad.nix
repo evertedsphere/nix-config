97db3e300861b1b2cf38ebdff3dce00de785373c
Soham Chowdhury <evertedsphere@gmail.com>
2023-05-12 19:09:11 +0200

    mullvad

---
+++ modules/nixos/mullvad.nix
@@ -0,0 +1,9 @@
+{pkgs, ...}:
+
+{
+      services.mullvad-vpn.enable = true;
+      # FIXME https://github.com/NixOS/nixpkgs/issues/113589
+      networking.firewall.checkReversePath = "loose";
+      networking.wireguard.enable = true;
+      environment.systemPackages = [ pkgs.mullvad-vpn ];
+}
