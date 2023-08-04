3ba763f17ee41b4b47862c910a2cc6fe377b2265
Soham Chowdhury <evertedsphere@gmail.com>
2023-08-04 08:53:07 +0200

    keyd: adapt to upstream config format change

--- modules/nixos/xserver/keyd.nix
+++ modules/nixos/xserver/keyd.nix
@@ -6,73 +6,14 @@
 }: {
   services.keyd = {
     enable = true;
-    ids = ["*"];
-    settings = {
-      main = {
-        capslock = "overload(control, esc)";
-        tab = "overload(meta, tab)";
-        # "q" = "q";
-        # "w" = "w";
-        # "e" = "f";
-        # "r" = "p";
-        # "t" = "b";
-        # "y" = "j";
-        # "u" = "l";
-        # "i" = "u";
-        # "o" = "y";
-        # "p" = ";";
-        # "[" = "[";
-        # "]" = "]";
-        # "a" = "a";
-        # "s" = "r";
-        # "d" = "s";
-        # "f" = "t";
-        # "g" = "g";
-        # "h" = "m";
-        # "j" = "n";
-        # "k" = "e";
-        # "l" = "i";
-        # ";" = "o";
-        # "'" = "'";
-        # "\\" = "\\";
-        # "z" = "x";
-        # "x" = "c";
-        # "c" = "d";
-        # "v" = "v";
-        # "b" = "z";
-        # "n" = "k";
-        # "m" = "h";
-        # "," = ",";
-        # "." = ".";
-        # "/" = "/";
-        # "f12" = "toggle(enable_lefthand)";
+    keyboards.default = {
+      ids = ["*"];
+      settings = {
+        main = {
+          capslock = "overload(control, esc)";
+          tab = "overload(meta, tab)";
+        };
       };
-    #   enable_lefthand = {
-    #     "space" = "overload(lefthand, space)";
-    #     "leftshift" = "overload(shift, backspace)";
-    #     "leftcontrol" = "enter";
-    #   };
-    #   lefthand = {
-    #     # qwfpb jluy;
-    #     # arstg mneio'\
-    #     # xcdvz kh,./
-    #     "q" = ";";
-    #     "w" = "y";
-    #     "e" = "u";
-    #     "r" = "l";
-    #     "t" = "j";
-    #     "a" = "o";
-    #     "s" = "i";
-    #     "d" = "e";
-    #     "f" = "n";
-    #     "g" = "m";
-    #     "h" = "'";
-    #     "z" = "/";
-    #     "x" = ".";
-    #     "c" = ",";
-    #     "v" = "h";
-    #     "b" = "k";
-    #   };
-      };
+    };
   };
 }
