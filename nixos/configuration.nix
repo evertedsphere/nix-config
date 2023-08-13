4f9cbcbc4e25ef321c1fd6fb4798b70b463be5dd
Soham Chowdhury <evertedsphere@gmail.com>
2023-08-13 10:09:00 +0200

    kernel: switch back from xanmod to zfs latest

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -74,7 +74,8 @@ in {
   boot.loader.efi.canTouchEfiVariables = true;
   boot.supportedFilesystems = ["zfs" "ntfs"];
   networking.hostId = "44a15ee1";
-  boot.kernelPackages = pkgs.linuxPackages_xanmod_latest; # config.boot.zfs.package.latestCompatibleLinuxPackages;
+  boot.zfs.enableUnstable = true;
+  boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
   boot.initrd.luks.devices."cryptroot".device = "/dev/disk/by-uuid/742e9b2a-dd82-4e82-b558-9508ccb6c9da";
   # boot.initrd.luks.devices."cryptdata".device = "/dev/disk/by-uuid/9ffbf99f-97b3-4931-9fbe-259a2b6498f3";
   boot.initrd.postDeviceCommands = lib.mkAfter ''
