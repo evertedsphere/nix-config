263ef3a13e8541f35e7088214fe54cfba63dae3d
Soham Chowdhury <evertedsphere@gmail.com>
2024-01-20 16:32:23 +0100

    [net] remove mullvad path collisions

--- modules/nixos/mullvad.nix
+++ modules/nixos/mullvad.nix
@@ -1,7 +1,9 @@
 {pkgs, ...}: {
-  services.mullvad-vpn.enable = true;
+  services.mullvad-vpn = {
+    enable = true;
+    package = pkgs.mullvad-vpn;
+  };
   # FIXME https://github.com/NixOS/nixpkgs/issues/113589
   networking.firewall.checkReversePath = "loose";
   networking.wireguard.enable = true;
-  environment.systemPackages = [pkgs.mullvad-vpn];
 }
