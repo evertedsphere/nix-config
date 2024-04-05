05e13b2011da8307b229beffa7dbcc9c500bc0fb
Soham Chowdhury <soham.chowdhury@tweag.io>
2024-04-05 12:18:30 +0200

    emacs: temporary fcitx hack

--- doom/config.input.el
+++ doom/config.input.el
@@ -4,6 +4,7 @@
 ;; In particular, this will switch to the main input method (for me, a standard
 ;; English (US) layout) when entering normal mode or in the minibuffer.
 (use-package! fcitx
+  :if (not (string= (system-name) "work"))
   :config
   (setq fcitx-remote-command "fcitx5-remote")
   (fcitx-aggressive-setup)
