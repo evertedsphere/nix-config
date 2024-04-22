75d0ec86312c706748908ee0ea27f1963b1a14cc
Soham Chowdhury <evertedsphere@gmail.com>
2024-04-22 23:31:23 +0200

    keyd: init lettermods

--- modules/nixos/keyd.nix
+++ modules/nixos/keyd.nix
@@ -9,11 +9,26 @@
     keyboards.default = {
       ids = ["*"];
       settings = {
-        main = {
+        main = let
+          idleTimeout = 130;
+          holdTimeout = 170;
+          overloadTimeout = 200;
+          # infuriatingly…unsuited to my usage
+          # lettermod = layer: key: "lettermod(${layer}, ${key}, ${builtins.toString idleTimeout}, ${builtins.toString holdTimeout})";
+          lettermod = layer: key: "overloadt(${layer}, ${key}, ${builtins.toString overloadTimeout})";
+        in {
           # This trick prevents random `esc`s in situations where that's undesirable.
           capslock = "timeout(overload(control, esc), 500, layer(control))";
           # Might as well do it for the other thing.
           tab = "timeout(overload(meta, tab), 500, layer(meta))";
+          a = lettermod "control" "a";
+          s = lettermod "shift" "s";
+          d = lettermod "alt" "d";
+          f = lettermod "meta" "f";
+          j = lettermod "meta" "j";
+          k = lettermod "alt" "k";
+          l = lettermod "shift" "l";
+          ";" = lettermod "control" ";";
         };
       };
     };
