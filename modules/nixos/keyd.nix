a6bc88d79d02b5ac9b0fadcd9ba83bbdd12fc976
Soham Chowdhury <evertedsphere@gmail.com>
2024-05-08 01:07:46 +0200

    keyd: move shift onto index fingers

--- modules/nixos/keyd.nix
+++ modules/nixos/keyd.nix
@@ -17,24 +17,39 @@
       ids = ["*"];
       settings = {
         main = let
-          overloadTimeout = 250;
-          lettermod = layer: key: "overloadt(${layer}, ${key}, ${builtins.toString overloadTimeout})";
-          abandonTimeout = 500;
           # This trick allows "cancelling" keypresses by holding them down for a while. This is particularly valuable for esc,
           # because an unwanted esc in various applications may abandon message composition or similar.
+          abandonTimeout = 500;
           modPrefMod = layer: key: "timeout(overload(${layer}, ${key}), ${builtins.toString abandonTimeout}, layer(${layer}))";
+
+          # home row mods
+          lettermod = timeout: layer: key: "overloadt(${layer}, ${key}, ${builtins.toString timeout})";
+          lettermod2 = timeout: layer: key: "overloadt2(${layer}, ${key}, ${builtins.toString timeout})";
+          shiftTimeout = 150;
+          defaultTimeout = 250;
         in {
+          "enter" = ";";
+
           capslock = modPrefMod "control" "esc";
           tab = modPrefMod "meta" "tab";
-          a = lettermod "control" "a";
-          s = lettermod "shift" "s";
-          d = lettermod "alt" "d";
-          f = lettermod "meta" "f";
-          j = lettermod "meta" "j";
-          k = lettermod "alt" "k";
-          l = lettermod "shift" "l";
-          ";" = lettermod "control" "enter";
-          "enter" = ";";
+
+          "g" = lettermod defaultTimeout "ime" "g";
+          "s" = lettermod defaultTimeout "meta" "s";
+          "l" = lettermod defaultTimeout "meta" "l";
+          ";" = lettermod defaultTimeout "control" "enter";
+          "a" = lettermod defaultTimeout "control" "a";
+          "f" = lettermod2 shiftTimeout "shift" "f";
+          "j" = lettermod2 shiftTimeout "shift" "j";
+          "k" = lettermod defaultTimeout "alt" "k";
+          "d" = lettermod defaultTimeout "alt" "d";
+        };
+
+        "ime" = {
+          "j" = "f13";
+          "k" = "f14";
+          "l" = "f15";
+          # why not
+          "h" = "esc";
         };
       };
     };
