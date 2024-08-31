5e37d5e9a69270226c4de0e58826e5d0eed257ff
Soham Chowdhury <evertedsphere@gmail.com>
2024-08-31 15:16:50 +0200

    emacs: switch back to stable

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -143,9 +143,20 @@ in {
   programs.doom-emacs = {
     enable = true;
     doomDir = ../doom;
-    emacs = pkgs.emacs-git;
+    # As of 31-08-2024, Emacs 31 is way snappier overall but also produces tons
+    # of multiple-second pauses doing god knows what (I couldn't be arsed to
+    # check with the profiler) and once sat there spinning for a whole five
+    # minutes trying to render my agenda. It's unusable.
+    # Also segfaults if you profile it sometimes.
+    emacs = pkgs.emacs;
   };
-  # services.emacs.enable = true;
+
+  # https://github.com/marienz/nix-doom-emacs-unstraightened/blob/cfd0a1ad0995efc8ee1149e09445bcde9f9b0a8d/README.md
+  # If you set `services.emacs.enable = true`, that will run Unstraightened as well
+  # (Unstraightened sets itself as `services.emacs.package`). Set
+  # `programs.doom-emacs.provideEmacs = false` or override `services.emacs.package`
+  # if you want a vanilla Emacs daemon instead.
+  services.emacs.enable = true;
 
   systemd.user.services.screenkey = let
     screenkey-script = pkgs.resholve.writeScript "run-screenkey" {
